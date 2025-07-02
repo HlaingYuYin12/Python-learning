dc0834ea0a7d2511c2d59593958f89dbc8d6a0de
HlaingYuYin <hlaingyuyin28@gmail.com>
2025-07-02 18:05:12 +0630

    lesson-5

---
+++ main.py
@@ -0,0 +1,15 @@
+print('hello world'); #print hello world
+
+# input('Name : ') #string type
+
+# print(input('Name : '))
+
+# name = print(input('Name : '))
+
+# age = input('tell me your age : ');
+# print('your age is '+age)
+
+PI = 3.14;
+r = int(input('radius : ')); #type cast
+area = PI*r**2;
+print(area);
