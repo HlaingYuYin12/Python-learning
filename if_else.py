71501adcdf2fb6774ec0b6e2aa0c8e20d1097f27
HlaingYuYin <hlaingyuyin28@gmail.com>
2025-07-02 18:46:05 +0630

    lesson8

---
+++ if_else.py
@@ -0,0 +1,16 @@
+# age = int(input('age: '))
+
+# if age < 18 :
+#     print('you are young') #indentation is important
+# elif age > 18 and age < 30 :
+#     print('you are normal age')
+# else :
+#     print('you are old')
+
+tired = input('are u tired? "y/n"')
+if tired == 'y' :
+    print('rest well')
+elif tired == 'n' :
+    print('go back to work')
+else :
+    print('please enter y or n')
